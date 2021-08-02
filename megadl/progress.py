202fa6afbdc57b2147e9345dbc6c01204cd485a6
Adrew <64140254+strange552@users.noreply.github.com>
2021-08-02 11:44:36 +0530

    Update progress.py

--- megadl/progress.py
+++ megadl/progress.py
@@ -35,7 +35,7 @@ async def progress_for_pyrogram(
             ''.join(["▱" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n🚀 **Speed:** {2}/s\n⏰ **Time Left:** {3}\n\n**© @AsmSafone | @SafoTheBot**".format(
+        tmp = progress + "{0} of {1}\n🚀 **Speed:** {2}/s\n⏰ **Time Left:** {3}\n\n**© @ImagerSupportGroup | @ImagerSupportGroup**".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
